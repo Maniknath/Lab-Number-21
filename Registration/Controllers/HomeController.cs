ff164b79cdebb0f454c26369b8e413ab6c0eff5c
maniknath <maniknath79@gmail.co>
2019-10-09 13:27:02 -0400

    2nd commit

---
+++ Registration/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Registration.Models;
+using System.Diagnostics;
+
+namespace Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult AddUser(User user)
+        {
+            ViewBag.Name = user.FirstName;
+            return View();
+        }
+    }
+}
